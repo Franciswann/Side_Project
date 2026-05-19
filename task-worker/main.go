1866561824dd7a1b1a4e46859668bab295363514
francis <a0975568423@gmail.com>
2026-05-19 18:07:58 +0800

    feat: implement worker pool for concurrent job processing

--- task-worker/main.go
+++ task-worker/main.go
@@ -15,16 +15,15 @@ func main() {
 	task1 := queue.Job{ID: 1, Payload: "task-1"}
 	task2 := queue.Job{ID: 2, Payload: "task-2"}
 	task3 := queue.Job{ID: 3, Payload: "task-3"}
+	task4 := queue.Job{ID: 4, Payload: "task-4"}
+	task5 := queue.Job{ID: 5, Payload: "task-5"}
 	q.Enqueue(task1)
 	q.Enqueue(task2)
 	q.Enqueue(task3)
+	q.Enqueue(task4)
+	q.Enqueue(task5)
 	close(q.JobChannel)
 
-	worker := worker.Worker{ID: 1}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		worker.Start(q)
-	}()
+	worker.StartPool(3, q, &wg)
 	wg.Wait()
 }
--- task-worker/worker/worker.go
+++ task-worker/worker/worker.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/Franciswann/Side_Project/task-worker/queue"
 )
@@ -13,5 +14,17 @@ type Worker struct {
 func (w *Worker) Start(q *queue.Queue) {
 	for task := range q.JobChannel {
 		fmt.Printf("Worker %v processing: %v\n", w.ID, task)
+	}
+}
+
+// Worker Pool
+func StartPool(numWorkers int, q *queue.Queue, wg *sync.WaitGroup) {
+	wg.Add(numWorkers)
+	for number := range numWorkers {
+		worker := Worker{ID: number + 1}
+		go func() {
+			defer wg.Done()
+			worker.Start(q)
+		}()
 	}
 }
